4760b4fc11e1c9e981ee260b15ce4679c4439eb3
Kelvin Souza Gonçalves <kelvinsouzag@gmail.com>
2024-08-27 11:24:42 -0300

    Inicialização projeto Spring

---
+++ src/main/java/br/com/expense/api/JwtExpenseApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.expense.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtExpenseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtExpenseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/expense/api/JwtExpenseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.expense.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtExpenseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
